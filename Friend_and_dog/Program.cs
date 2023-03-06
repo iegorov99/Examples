4cbf637c0955c7059029ff2759244b722ceb640e
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-06 14:58:02 +0300

    Friends and Dog

---
+++ Friend_and_dog/Program.cs
@@ -0,0 +1,25 @@
+﻿int count =  0;
+int distance = 1000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time;
+while(distance > 10)
+{
+    if(friend>1)
+    {
+        time = distance/(firstFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    else
+    {
+        time = distance/(secondFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    distance = distance - (firstFriendSpeed+secondFriendSpeed) * time;
+    count++;
+}
+Console.Write("Собака пробежала ");
+Console.Write(count);
+Console.WriteLine(" раз.");
