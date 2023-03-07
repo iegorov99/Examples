c7a6bde0eea9a34bcb801599b7b94ce394155350
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-07 08:06:52 +0300

    Частное двух чисел

---
+++ Div/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double numberB = Convert.ToDouble(Console.ReadLine());
+double div = numberA / numberB;
+Console.WriteLine(div);
