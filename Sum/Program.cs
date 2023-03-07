120762b4fa3ec2f156382d3019cd3fb3ab878ab9
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-07 08:01:20 +0300

    Сумма двух чисел

---
+++ Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int sum = numberA + numberB;
+Console.WriteLine(sum);
