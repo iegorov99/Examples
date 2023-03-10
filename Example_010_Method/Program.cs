e6dc5aad13c483a616e444ced82a3545e66d8abe
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-10 09:27:01 +0300

    поиск индекса числа в массиве

---
+++ Example_010_Method/Program.cs
@@ -0,0 +1,15 @@
+﻿//               0   1   2   3   4   5   6   7   8
+int[] array = { 19, 62, 35, 49, 55, 76, 73, 82, 19 };
+int n = array.Length;
+int find = 19;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    
+    index++;
+}
