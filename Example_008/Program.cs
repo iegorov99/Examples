18dd4e57a0e3fb45319accfdfa5cea53ca1a1850
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-10 09:25:53 +0300

    Функция

---
+++ Example_008/Program.cs
@@ -0,0 +1,24 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 19;
+int b1 = 32;
+int c1 = 73;
+int a2 = 494;
+int b2 = 65;
+int c2 = 46;
+int a3 = 71;
+int b3 = 82;
+int c3 = 19;
+
+/*int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+*/
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.WriteLine(max);
